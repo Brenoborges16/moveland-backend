8e996c322e64fabb2f8f7ded0648860746a8a185
Fabiola Borges <fabiolab2008@gmail.com>
2025-04-14 20:42:16 -0300

    🚀 Backend inicial do MoveLand

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('🚀 MoveLand backend no ar!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
